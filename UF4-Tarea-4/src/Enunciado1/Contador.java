64d1ff98f83a148e3c3c21a3330be9971851e6c6
fernando.sanchez <fsm_031@hotmail.com>
2015-11-12 10:23:23 +0100

    Comentario del ejercicio 1 (ENUNCIADO 1): se ha comentado el código de las clases Contador y Vocales.

--- UF4-Tarea-4/src/Enunciado1/Contador.java
+++ UF4-Tarea-4/src/Enunciado1/Contador.java
@@ -1,25 +1,39 @@
 package Enunciado1;
+
 /**
  * @author Pedro J. Camacho
  * @tarea Debes comentar adecuadamente el código
  *
+ *        FERNANDO SÁNCHEZ MARTÍNEZ - COMENTARIO DE CÓDIGO
+ *
  */
 public class Contador {
-	private int c = 0;
+	private int c = 0; // Se crea una variable int que representa el numero de
+						// vocales y sera nuestro contador. Se inicializa a 0.
 
+	/**
+	 * se crea el constructor de la clase que recibe por parámetro el contador.
+	 * 
+	 * @param num
+	 */
 	public Contador(int num) {
 		this.c = num;
 	}
 
+	/**
+	 * se crean 3 metodos sincronizados para evitar errores en el contador, al
+	 * ser una variable comparida por muchos hilos.
+	 */
 	public synchronized void increment() {
-		c++;
+		c++;// suma el contador +1.
 	}
 
 	public synchronized void decrement() {
-		c--;
+		c--;// resta el contador -1.
 	}
 
 	public synchronized int value() {
-		return c;
+		return c;// proporciona el valor del contador en el momento de ser
+					// llamado.
 	}
 }
--- UF4-Tarea-4/src/Enunciado1/Vocales.java
+++ UF4-Tarea-4/src/Enunciado1/Vocales.java
@@ -1,40 +1,76 @@
 package Enunciado1;
+
 import java.util.Scanner;
+
 /**
  * @author Pedro J. Camacho
  * @tarea Debes comentar adecuadamente el código
  *
+ *   FERNANDO SÁNCHEZ MARTÍNEZ - COMENTARIO DE CÓDIGO
+ *
+ */
+
+/**
+ * Ésta clase representa el hilo
+ * 
+ * @author fernando.sanchez
+ *
  */
 class Rastrea extends Thread {
-	private Contador cont;
-	private String texto;
-	private char letra;
+	private Contador cont;// variable contador que se igualara a la de la clase
+							// contador
+	private String texto;// string que representa el txt
+	private char letra;// caracteres
 
+	/**
+	 * se crea el contructor y se le pasan por parámetro las variables creadas
+	 * anteriormente
+	 * 
+	 * @param cont
+	 * @param texto
+	 * @param letra
+	 */
 	public Rastrea(Contador cont, String texto, char letra) {
 		this.cont = cont;
 		this.texto = texto;
 		this.letra = letra;
 	}
 
+	/**
+	 * Método encargado de las acciones a la hora de ejecutar un hilo.
+	 */
 	public void run() {
+		// se recorre el texto.
 		for (int i = 0; i < texto.length(); i++)
+			// si el texto en el caracter correspondiente en el bucle es igual a
+			// la letra, incrementar contador.
 			if (texto.charAt(i) == letra)
 				cont.increment();
 	}
 }
 
+/**
+ * Clase main que crea y ejecuta los distintos hilos.
+ * 
+ * @author fernando.sanchez
+ *
+ */
 public class Vocales {
 	public static void main(String[] args) {
-		String miTexto;
+		String miTexto;// string que representa el texto
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Introduzca el texto: ");
-		miTexto = sc.nextLine();
-		Contador contador = new Contador(0);
+		miTexto = sc.nextLine();// se recoge en el string el texto introducido
+								// por teclado
+		Contador contador = new Contador(0);// se crea un objeto contador
+											// inicializado a 0
+		// se crean nª de hilos correspondientes por cada vocal
 		Thread contadorA = new Rastrea(contador, miTexto, 'a');
 		Thread contadorE = new Rastrea(contador, miTexto, 'e');
 		Thread contadorI = new Rastrea(contador, miTexto, 'i');
 		Thread contadorO = new Rastrea(contador, miTexto, 'o');
 		Thread contadorU = new Rastrea(contador, miTexto, 'u');
+		// se lanzan los hilos
 		contadorA.start();
 		contadorE.start();
 		contadorI.start();
@@ -49,6 +85,7 @@ public class Vocales {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		// se imprime el numero de vocales recogidas de nuestro texto.
 		System.out.print("El número de vocales del texto es: "
 				+ contador.value());
 	}
